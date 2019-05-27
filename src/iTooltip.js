6228112bce8c43e7219b120f7a40f6c23faeaeba
Insane <shagode@ukr.net>
2019-05-27 18:56:46 +0300

    Added "moved" method.

--- src/iTooltip.js
+++ src/iTooltip.js
@@ -33,11 +33,37 @@ export default class iTooltip {
         elem.removeAttribute('title')
 
         document.body.appendChild(this.tooltip)
+
+        elem.addEventListener('mousemove', event => this.moved(event))
     }
 
     removeTooltip (event) {
         event.target.setAttribute('title', this.tooltip.innerHTML)
         this.tooltip.remove()
+    }
+
+    moved (event) {
+        let tooltipClientRect = this.tooltip.getBoundingClientRect()
+        let tooltipWidth = tooltipClientRect.right - tooltipClientRect.left,
+            tooltipHeight = tooltipClientRect.bottom - tooltipClientRect.top,
+            rightEdge = document.documentElement.clientWidth - event.clientX,
+            bottomEdge = document.documentElement.clientHeight - event.clientY
+        
+        if (rightEdge <= tooltipWidth) {
+            this.tooltip.style.left = null
+            this.tooltip.style.right = `${rightEdge - this.settings.indentX}px`
+        } else {
+            this.tooltip.style.right = null
+            this.tooltip.style.left = `${event.clientX - this.settings.indentX}px`
+        }
+
+        if (bottomEdge <= tooltipHeight) {
+            this.tooltip.style.top = null
+            this.tooltip.style.bottom = `${bottomEdge}px`
+        } else {
+            this.tooltip.style.bottom = null
+            this.tooltip.style.top = `${event.clientY - this.settings.indentY}px`
+        }
     }
 }
 
